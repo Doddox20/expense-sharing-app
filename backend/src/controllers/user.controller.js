dbb8e6bbad09c4b1f79fdea3ed93f2933ad127fd
Doddox20 <dorian.contal20@gmail.com>
2026-04-05 13:34:17 +0200

    Fonction User findById

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -10,6 +10,16 @@ const userController = {
       res.status(500).json({ error: 'Database error', details: err.message });
     }
   },
+  async getUserById(req, res) {
+    try {
+      const user = await userService.getUserById(req.params.id);
+      res.json(user);
+    } catch (err) {
+      console.error('DB ERROR:', err);
+      res.status(500).json({ error: 'Database error', details: err.message });
+    }
+  },
+
 };
 
 export default userController;
--- backend/src/repositories/user.repository.js
+++ backend/src/repositories/user.repository.js
@@ -5,6 +5,10 @@ const userRepository = {
     const result = await pool.query('SELECT * FROM users');
     return result.rows;
   },
+  async findById(id) {
+    const result = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
+    return result.rows[0];
+  }
 };
 
 export default userRepository;
